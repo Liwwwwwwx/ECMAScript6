1a3318304f1b0da1c117d708c7073dff72e3f7b9
“Liwwwwwwx” <1076843408@qq.com>
2022-07-29 17:01:42 +0800

    两个有序数组合并,若数组长度为奇数,则返回中间值;若数组长度为偶数,则返回平均值

---
+++ Code/136.find_median_of_two_sorted_array.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} arr1 - sorted integer array
+ * @param {number[]} arr2 - sorted integer array
+ * @returns {number}
+ */
+function median(arr1, arr2) {
+    // your code here
+    let arr = [...arr1, ...arr2]
+
+    // 冒泡排序
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                let temp = arr[i]
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    var midNumber = 0
+    var mid = arr.length / 2
+    if (arr.length % 2 == 0) {
+        midNumber = (arr[mid] + arr[mid - 1]) / 2
+    } else {
+        midNumber = arr[(arr.length - 1) / 2]
+    }
+
+    return midNumber
+}
